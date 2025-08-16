9866d147df12fe61a8168dcd07190b364b9792db
Pablo <pleyva2004@gmail.com>
2025-08-16 18:35:16 -0400

    removing unused dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Sidebar from './components/Sidebar';
 import Hero from './components/Hero';
 import FloatingCTA from './components/FloatingCTA';
